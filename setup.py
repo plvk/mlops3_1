3e502d8485665d588388e6dccb0b8d1de7e82a2c
plvk <96695657+plvk@users.noreply.github.com>
2025-01-30 04:58:07 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mlops3_1",
+    version="0.1",
+    description="Credit default analysis package",
+    author="Michael Ivanov",
+    packages=find_packages(),
+    install_requires=[
+        # не забыть указать зависимости
+    ],
+    python_requires=">=3.11",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
 
